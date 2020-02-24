c267fcd773d02d1dfe346e6d8c8ee14748147eeb
devkudasov <dev.kudasov@gmail.com>
2020-02-24 14:32:41 +0300

    Module 1 assignment lab

---
+++ dev283x/src/module_2/converter.js
@@ -0,0 +1,21 @@
+const csvToJson = data => {
+  return new Promise((resolve, reject) => {
+    try {
+      const rows = data.split('\r\n');
+      const keys = rows[0].split(',');
+
+      resolve(rows.slice(1).reduce((acc, row, index) => {
+        const values = row.split(',');
+        acc[index] = {};
+        values.forEach((value, valueIndex) => acc[index][keys[valueIndex]] = value);
+        return acc;
+      }, []));
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = {
+  csvToJson
+};
---
+++ dev283x/src/module_2/index.js
@@ -0,0 +1,17 @@
+const https = require('https');
+const converter = require('./converter');
+
+const fetch = (url = 'https://courses.edx.org/assets/courseware/v1/07d100219da1a726dad5eddb090fa215/asset-v1:Microsoft+DEV283x+3T2018+type@asset+block/customer-data.csv') => {
+  return new Promise((resolve, reject) => {
+    https.get(url, response => {
+      let data = '';
+      response.on('data', chunk => data += chunk);
+      response.on('end', () => resolve(data));
+    }).on('error', reject);
+  });
+};
+
+fetch(process.argv[2])
+  .then(converter.csvToJson)
+  .then(console.log)
+  .catch(console.error);
